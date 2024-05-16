1dab9bd26ebb6ee1dc56e8ea4efeab8d4a4bd2ef
RayThary <ghfjtorl121291@gmail.com>
2024-05-16 14:57:25 +0900

    0516

--- Assets/Script/UI/MainUI.cs
+++ Assets/Script/UI/MainUI.cs
@@ -1,24 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainUI : MonoBehaviour
 {
-    private Button m_btnStart;
-    private Button m_btnOption;
-    private Button m_btnEnd;
+
+    [SerializeField] private GameObject m_btnParent;
+
+    [SerializeField] private Button m_btnStart;
+    [SerializeField] private Button m_btnOption;
+    [SerializeField] private Button m_btnOptionClose;
+    [SerializeField] private Button m_btnEnd;
+
 
     private GameObject m_optinWindow;
 
     void Start()
     {
-        
+        m_btnParent = transform.Find("Button").gameObject;
+        m_optinWindow = transform.Find("OptionWindow").gameObject;
+
+        m_btnStart = m_btnParent.transform.Find("Start").GetComponent<Button>();
+        m_btnOption = m_btnParent.transform.Find("Option").GetComponent<Button>();
+        m_btnEnd = m_btnParent.transform.Find("Exit").GetComponent<Button>();
+        m_btnOptionClose = m_optinWindow.transform.Find("OptionClose").GetComponent<Button>();
+
+        m_btnStart.onClick.AddListener(btnStart);
+        m_btnOption.onClick.AddListener(btnOption);
+        m_btnEnd.onClick.AddListener(btnEnd);
+        m_btnOptionClose.onClick.AddListener(btnOptionClose);
+
+
+        m_optinWindow.SetActive(false);
+
     }
 
-    
-    void Update()
+    private void btnStart()
     {
-        
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    private void btnOption()
+    {
+        if (m_optinWindow.activeSelf == false)
+        {
+            m_optinWindow.SetActive(true);
+        }
+    }
+
+    private void btnOptionClose()
+    {
+        if (m_optinWindow.activeSelf == true)
+        {
+            m_optinWindow.SetActive(false);
+        }
+    }
+
+    private void btnEnd()
+    {
+        Application.Quit();
     }
 }
