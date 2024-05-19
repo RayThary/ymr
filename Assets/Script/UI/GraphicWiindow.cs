f6a1c58b2ed21ae2b6819cc2a4a04baabe132777
DESKTOP-TTSUQC3\duqor <duqorghg@gmail.com>
2024-05-19 19:08:54 +0900

    그래픽 설정 추가

--- Assets/Script/UI/GraphicWiindow.cs
+++ Assets/Script/UI/GraphicWiindow.cs
@@ -1,18 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GraphicWiindow : MonoBehaviour
 {
+    FullScreenMode fullScreenMode;
+    [SerializeField]
+    private Dropdown dropdown;
+    [SerializeField]
+    private Toggle toggle;
+    private int value;
+    List<Resolution> resolutions = new ();
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitUI();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void InitUI()
+    {
+        for(int i = 0; i < Screen.resolutions.Length; i++)
+        {
+            if (Screen.resolutions[i].refreshRateRatio.value >= 60)
+            {
+                resolutions.Add(Screen.resolutions[i]);
+            }
+        }
+
+        dropdown.options.Clear();
+        foreach (Resolution resolution in resolutions)
+        {
+            Dropdown.OptionData optionData = new Dropdown.OptionData();
+            optionData.text = resolution.width + " x " + resolution.height + " " + resolution.refreshRateRatio.value + "Hz";
+            dropdown.options.Add(optionData);
+
+            if(resolution.width == Screen.width && resolution.height == Screen.height)
+            {
+                dropdown.value = value;
+            }
+            value++;
+        }
+        dropdown.RefreshShownValue();
+
+        toggle.isOn = Screen.fullScreenMode.Equals(FullScreenMode.FullScreenWindow) ? true : false;
+    }
+
+    public void DropdownValueChange(int _value)
+    {
+        value = _value;
+    }
+
+    public void FullScreenBox(bool isFull)
+    {
+        fullScreenMode = isFull ? FullScreenMode.FullScreenWindow : FullScreenMode.Windowed;
+    }
+
+    public void OKButton()
+    {
+        Screen.SetResolution(resolutions[value].width, resolutions[value].height, fullScreenMode);
     }
 }
